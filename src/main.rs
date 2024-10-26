6b34360b0abc6675d88629980b9864d8a741a177
CommName <17132081+CommName@users.noreply.github.com>
2024-10-26 23:09:17 +0200

    Removed rw lock for moov

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,11 @@ mod file_sink;
 fn ws(
     ws: WebSocket,
     recv: Data<& tokio::sync::broadcast::Sender<Vec<u8>>>,
-    moov: Data<& Arc<RwLock<Vec<Vec<u8>>>>>
+    Data(moov): Data<&Arc<Vec<Vec<u8>>>>
 ) -> impl IntoResponse {
     let mut receiver = recv.subscribe();
-    let moov = moov.0.read().unwrap().clone();
+
+    let moov = Arc::clone(moov);
     ws.on_upgrade(move |socket| async move {
         let (mut sink, mut stream) = socket.split();
         for pack in moov.iter() {
@@ -45,12 +46,6 @@ fn ws(
     })
 }
 
-pub enum StateOfMp4 {
-    moof,
-    mdat,
-    data,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Initialize GStreamer
@@ -63,32 +58,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (tx, _) = tokio::sync::broadcast::channel::<Vec<u8>>(1024);
     let tx2 = tx.clone();
 
-    let moov: Arc<RwLock<Vec<Vec<u8>>>> = Arc::new(RwLock::new(Vec::new()));
-    let moov2 = Arc::clone(&moov);
+    
+    
+    // Start playing
+    pipeline.set_state(State::Playing)?;
+    println!("Pipline started");
+    
+    let mut moov: Vec<Vec<u8>> = Vec::new();
+    println!("Saving header");
+    let mut number_of_buffs = 0;
+    while number_of_buffs < 2 {
+        if let Ok(buffer) = recv.recv() {
+            let mapa = buffer.map_readable().unwrap();
+            let slice = mapa.to_vec();
+            moov.push(slice);
+            number_of_buffs += 1;
+        } else {
+            break;
+        }
+    }
+    let moov = Arc::new(moov);
 
 
     std::thread::spawn(move || {
         let mut pts = None;
         let mut vec = Vec::with_capacity(1024);
-        let mut number_of_buffs = 0;
-        {
-            println!("Starting header");
-            let mut moov = moov.write().unwrap();
-            while number_of_buffs < 2 {
-                if let Ok(buffer) = recv.recv() {
-                    let mapa = buffer.map_readable().unwrap();
-                    let slice = mapa.to_vec();
 
-                    moov.push(slice);
-                    number_of_buffs += 1;
-                } else {
-                    break;
-                }
-            }
-        }
 
         println!("Started recv");
-        let mut state = StateOfMp4::moof;
         while let Ok(buffer) = recv.recv() {
 
             println!("pts: {:?}", pts);
@@ -96,13 +93,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let mapa = buffer.map_readable().unwrap();
             let mut slice = mapa.to_vec();
-            //println!("Buffer {:?}", buffer);
             // moof 6d6f 6f66
-            // mdat 6d64 6174
-
             if slice[4] == 0x6d && slice[5] == 0x6f && slice[6] == 0x6f && slice[7] == 0x66 {
                 tx.send(vec.clone());
-                // TODO send vector
                 vec.clear();
 
             }
@@ -113,32 +106,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
 
-    // Start playing
-    pipeline.set_state(State::Playing)?;
-    println!("Pipline started");
-
-    // // Wait until error or EOS
-    // let bus = pipeline.bus().unwrap();
-    // for msg in bus.timed_pop_filtered(gstreamer::ClockTime::NONE, &[gstreamer::MessageType::Eos, gstreamer::MessageType::Error]) {
-    //     match msg.view() {
-    //         gstreamer::MessageView::Error(err) => {
-    //             eprintln!("Error received from element {:?}: {}", msg.src().map(|s| s.name()), err.error());
-    //             eprintln!("Debugging information: {:?}", err.debug());
-    //             break;
-    //         }
-    //         gstreamer::MessageView::Eos(..) => {
-    //             println!("End of stream reached.");
-    //             break;
-    //         }
-    //         _ => (),
-    //     }
-    // }
 
     let app = Route::new()
         .at("/ws",
             get(ws)
             .data(tx2)
-            .data(moov2)
+            .data(moov)
         );
 
     let res = Server::new(TcpListener::bind("0.0.0.0:3000"))
