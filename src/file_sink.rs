5d63b75e199bb3fc5d0a22683c4208e655da6f0b
CommName <17132081+CommName@users.noreply.github.com>
2024-10-29 22:43:04 +0100

    Removing old files

--- src/file_sink.rs
+++ src/file_sink.rs
@@ -1,11 +1,12 @@
-use std::{str::FromStr, sync::Arc};
+use std::{path::PathBuf, str::FromStr, sync::Arc, time::SystemTime};
 
 use tokio::{fs::File, io::AsyncWriteExt, sync::broadcast::Receiver};
 
 use crate::ParsedBuffer;
 
-const CREATE_NEW_FILE_THRESHOLD: u64 = 30;
-
+const CREATE_NEW_FILE_THRESHOLD: u64 = 2;
+const MAX_NUMBER_OF_FILES: u64 = 3; // 24 * 60 * 60 / CREATE_NEW_FILE_THRESHOLD; // 1 - day
+const APP_DATA_PATH: &str = "./";
 
 pub async  fn file_saver(mut recv: Receiver<Arc<ParsedBuffer>>, moov: Arc<Vec<Vec<u8>>>) {
 
@@ -18,7 +19,9 @@ pub async  fn file_saver(mut recv: Receiver<Arc<ParsedBuffer>>, moov: Arc<Vec<Ve
 
     while let Ok(buffer) = recv.recv().await {
         if buffer.key_frame && should_create_new_file(&buffer,&mut timestamp_when_file_is_created) {
-            // TODO: rotate file
+            while should_file_be_rotated().await {
+                remove_oldest_file().await;
+            }
 
             file = generate_new_file().await;
             if let Err(_) = save_moov_header(&moov, &mut file).await {
@@ -30,6 +33,46 @@ pub async  fn file_saver(mut recv: Receiver<Arc<ParsedBuffer>>, moov: Arc<Vec<Ve
         }
     }
 
+}
+
+async fn remove_oldest_file() {
+    let mut files = tokio::fs::read_dir(APP_DATA_PATH).await.unwrap();
+
+    let mut max_time = SystemTime::now();
+    let mut path = PathBuf::new();
+    while let Ok(Some(file)) = files.next_entry().await {
+        if let Ok(metadata) = file.metadata().await {
+            if metadata.is_file() && file.path().extension().map(|e| e == "mp4").unwrap_or(false) {
+                let time_created = metadata.created().unwrap();
+
+                if time_created < max_time {
+                    max_time = time_created;
+                    path = file.path();
+                }
+            }
+        }
+    }
+
+    if let Err(_) = tokio::fs::remove_file(path).await {
+        // TODO log error and handle it
+    }
+
+}
+
+async fn should_file_be_rotated() -> bool {
+    let mut files = tokio::fs::read_dir(APP_DATA_PATH).await.unwrap();
+
+    let mut number_of_mp4_files = 0;
+    while let Ok(Some(file)) = files.next_entry().await {
+        if file.file_name()
+            .to_str()
+            .map(|s| s.ends_with(".mp4"))
+            .unwrap_or(false) {
+
+            number_of_mp4_files += 1;
+        }
+    }
+    number_of_mp4_files > MAX_NUMBER_OF_FILES
 }
 
 fn should_create_new_file(buffer: &Arc<ParsedBuffer>, base_timestamp: &mut u64) -> bool {
@@ -57,7 +100,7 @@ fn generate_file_name() -> String {
 
 async fn generate_new_file() -> File {
     let file_name = generate_file_name();
-    let file_path = std::path::PathBuf::from_str(&format!("./{file_name}")).unwrap();
+    let file_path = std::path::PathBuf::from_str(&format!("{APP_DATA_PATH}/{file_name}")).unwrap();
     File::create_new(file_path)
         .await
         .unwrap() // TODO: Handle errors
