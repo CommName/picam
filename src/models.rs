7df09026517de590f41deb05bf0f97b50cb8efc2
CommName <17132081+CommName@users.noreply.github.com>
2025-03-03 22:07:41 +0100

    Storage for pipline config

--- src/models.rs
+++ src/models.rs
@@ -7,3 +7,12 @@ pub struct User {
     pub username: String,
     pub password: String
 }
+
+
+#[derive(Debug, Clone)]
+pub struct PipelineConfig {
+    pub source: Option<String>,
+    pub use_cam_builtin_encoder: Option<bool>,
+    pub width: Option<i32>,
+    pub height: Option<i32>
+}
--- src/storage/sqlite.rs
+++ src/storage/sqlite.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
-use sqlx::{migrate::MigrateDatabase, sqlite::SqliteRow, Row, Sqlite, SqlitePool};
+use serde::{de::{value, DeserializeOwned}, Deserialize, Serialize};
+use sqlx::{migrate::MigrateDatabase, sqlite::SqliteRow, Executor, Row, Sqlite, SqlitePool};
 
-use super::{CameraConfigStorage, UserStorage};
+use super::{PipelineConfigStorage, UserStorage};
 use crate::models::*;
 use log::*;
 
@@ -37,7 +38,7 @@ pub struct SQLiteUserStorage {
     db: Arc<SqlitePool>
 }
 
-fn userFromRow(r: SqliteRow) -> User {
+fn user_from_row(r: SqliteRow) -> User {
     User {
         username: r.get::<String, &str>("username"),
         password: r.get::<String, &str>("password")
@@ -56,10 +57,10 @@ impl UserStorage for SQLiteUserStorage {
                 Vec::new()}
             );
 
-        res.into_iter().map(userFromRow).collect()
+        res.into_iter().map(user_from_row).collect()
     }
     async fn get_user(&self, username: &str) -> Option<User> {
-        Some(userFromRow(sqlx::query("SELECT username, password from users where username = $1;")
+        Some(user_from_row(sqlx::query("SELECT username, password from users where username = $1;")
             .bind(username)
             .fetch_one(self.db.as_ref())
             .await
@@ -102,6 +103,77 @@ pub struct SqliteCameraConfigStorage {
     db: Arc<SqlitePool>
 }
 
-impl CameraConfigStorage for SqliteCameraConfigStorage {
 
+async fn fetch_config<T: DeserializeOwned>(key: &str, db: &SqlitePool) -> Option<T>{
+    let row = sqlx::query("SELECT value FROM pipelineconfig WHERE key = $1")
+        .bind(key)
+        .fetch_one(db)
+        .await;
+    match row {
+        Ok(sql_row) => {
+            let json: String = sql_row.get("width");
+            serde_json::from_str(json.as_str()).ok()
+        },
+        Err(sqlx::Error::RowNotFound) => {
+            None
+        }
+        Err(e) => {
+            error!("Error fetching pipline config (width): {e:?}");
+            None
+        }
+    }
+}
+
+async fn set_config<T: Serialize>(key: &str, value: &T, db: &SqlitePool ) {
+    sqlx::query("INSERT INTO pipelineconfig(key, value) VALUE($1, $2")
+        .bind(key)
+        .bind(&serde_json::to_string(value).unwrap())
+        .execute(db);
+}
+
+async fn unset_config(key: &str, db: &SqlitePool) {
+    sqlx::query("DELETE FROM pipelineconfig where key=$1")
+        .bind(key)
+        .execute(db);
+}
+
+async  fn update_paramter<T:Serialize>(key: &str, value: &Option<T>, db: &SqlitePool) {
+    unset_config(key, db).await;
+    if let Some(value) = value {
+        set_config(key, value, &db).await;
+    }
+}
+
+const WIDTH: &str = "width";
+const HEIGHT: &str = "height";
+const USE_BUILT_IN_ENCODER: &str = "use_cam_built_in_encoder";
+const SOURCE: &str = "source";
+#[async_trait::async_trait]
+impl PipelineConfigStorage for SqliteCameraConfigStorage {
+
+    async fn width(&self) -> Option<i32> {
+        fetch_config(WIDTH, self.db.as_ref()).await
+    }
+    async fn height(&self) -> Option<i32> {
+        fetch_config(HEIGHT, self.db.as_ref()).await
+    }
+    async fn use_cam_builtin_encoder(&self) -> Option<bool> {
+        fetch_config(USE_BUILT_IN_ENCODER, self.db.as_ref()).await
+    }
+    async fn source(&self) -> Option<String> {
+        fetch_config(SOURCE, self.db.as_ref()).await
+    }
+
+    async fn set_width(&self, value: Option<i32>) {
+        update_paramter(WIDTH, &value, &self.db).await;
+    }
+    async fn set_height(&self, value: Option<i32>) {
+        update_paramter(HEIGHT, &value, &self.db).await;
+    }
+    async fn set_use_cam_builtin_encoder(&self, value: Option<bool>) {
+        update_paramter(USE_BUILT_IN_ENCODER, &value, &self.db).await;
+    }
+    async fn set_source(&self, value: Option<&str>) {
+        update_paramter(SOURCE, &value, &self.db).await;
+    }
 }
